1ddba3d09138ea1b547168d2cf6bd492d21c9e0c
Мирон Пожарский <miron2801@bk.ru>
2022-07-04 14:29:26 +0400

    add test

--- tests/ddos.py
+++ tests/ddos.py
@@ -2,27 +2,33 @@ import requests
 import random
 from dataclasses import dataclass
 import json
+from threading import Thread
+import time
+
+COUNT_TREADS = 100
 
 
-@dataclass
-class Point:
-    x: int
-    y: int
-    color: str
+def treading_function(data):
+    time.sleep(5)
+    print("Thread " + str(data) + " started")
+    for x in range(100):
+        for y in range(100):
+            color = lambda: random.randint(0, 255)
+            r = requests.post(
+                "http://localhost:5000/pixel",
+                json={
+                    "cord": [x, y],
+                    "color": "#%02X%02X%02X" % (color(), color(), color()),
+                },
+            )
+            # print(r.text, r.status_code, [x, y])
 
 
-def make_request(point):
-    r = requests.post(
-        "http://192.168.0.157:5000/pixel",
-        json={"cord": [point.x, point.y], "color": point.color},
-    )
-    return r
-
-
-for x in range(100):
-    for y in range(100):
-        r = lambda: random.randint(0, 255)
-        data = make_request(Point(x, y, "#%02X%02X%02X" % (r(), r(), r())))
-        print(data.text, data.status_code, [x, y])
-
-test_incorrect_HEX()
+threads = []
+for i in range(COUNT_TREADS):
+    try:
+        t = Thread(target=treading_function, args=(i,))
+        threads.append(t)
+        t.start()
+    except:
+        print("Error: unable to start thread")
